29b1c8b9053689b4f934e2d1f54b8e968e8c424b
Anton Karmanov <a.karmanov@inventati.org>
2021-11-10 23:16:01 +0300

    Fix db role Molecule verifying

--- ansible/roles/db/molecule/default/tests/test_default.py
+++ ansible/roles/db/molecule/default/tests/test_default.py
@@ -6,15 +6,20 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-# check if MongoDB is enabled and running
 def test_mongo_running_and_enabled(host):
+    ''' MongoDB is enabled and running '''
     mongo = host.service("mongod")
     assert mongo.is_running
     assert mongo.is_enabled
 
 
-# check if configuration file contains the required line
 def test_config_file(host):
+    ''' Configuration file contains the required line '''
     config_file = host.file('/etc/mongod.conf')
     assert config_file.contains('bindIp: 0.0.0.0')
     assert config_file.is_file
+
+
+def test_socket_is_binded(host):
+    ''' Port is listened '''
+    assert host.socket('tcp://0.0.0.0:27017').is_listening
